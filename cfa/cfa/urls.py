42782c6d93088803f836ee752697d2d9a06f1722
Peter-Binate <peter.binate@gmail.com>
2024-06-20 09:21:47 +0200

    add student register feature with token

--- cfa/cfa/urls.py
+++ cfa/cfa/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from cfaRegister.views import CfaUserViewSet, LoginView, InvitationViewSet
+from cfaRegister.views import CfaUserViewSet, LoginView, InvitationViewSet, StudentRegisterView
 
 router = DefaultRouter()
 router.register(r'register-cfa', CfaUserViewSet, basename='register-cfa')
@@ -12,4 +12,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
+    path('student-register/', StudentRegisterView.as_view(), name='student-register'),
 ]
